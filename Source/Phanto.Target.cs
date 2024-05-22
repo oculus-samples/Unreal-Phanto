920748b0f0d554af8b368350405aa03a18915746
jasonmeisel <jason.meisel@gmail.com>
2024-05-21 19:01:50 -0700

    Upgrade build version to fix compilation errors for 5.4

--- Source/Phanto.Target.cs
+++ Source/Phanto.Target.cs
@@ -8,7 +8,8 @@ public class PhantoTarget : TargetRules
 	public PhantoTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
+		DefaultBuildSettings = BuildSettingsVersion.V4;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_3;
 
 		ExtraModuleNames.AddRange( new string[] { "Phanto" } );
 	}
