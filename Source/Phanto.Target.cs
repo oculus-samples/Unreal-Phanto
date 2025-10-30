431d3f7f93edadfda4ff8640fa650a8240e91468
Alexandre Thivierge <athivierge@meta.com>
2025-10-30 10:01:25 -0700

    Update to UE 5.5

--- Source/Phanto.Target.cs
+++ Source/Phanto.Target.cs
@@ -8,8 +8,7 @@ public class PhantoTarget : TargetRules
 	public PhantoTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V4;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_3;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
 
 		ExtraModuleNames.AddRange( new string[] { "Phanto" } );
 	}
--- Source/PhantoEditor.Target.cs
+++ Source/PhantoEditor.Target.cs
@@ -8,8 +8,7 @@ public class PhantoEditorTarget : TargetRules
 	public PhantoEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V4;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_3;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
 
 		ExtraModuleNames.AddRange( new string[] { "Phanto" } );
 	}
